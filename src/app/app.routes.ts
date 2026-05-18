975eaad318dd7d49f7d57f52069bed3077a58537
zookat <zotova.ea@gmail.com>
2026-05-18 10:23:03 +0200

    add routing and components for movie app; update home page title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { MovieList } from './features/movie-list/movie-list';
+import { MovieDetails } from './features/movie-details/movie-details';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home }, // ruta raíz: '' al componente "Home",
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // ruta de redirección al componente "Home". Trabaja el atributo pathMatch.
+  { path: 'movies', component: MovieList }, // ruta 'movies' al componente MovieList,
+  { path: 'movies/:movieId', component: MovieDetails }, // ruta dinámica: 'movies/:movieId' al componente MovieDetails,
+  { path: '**', component: NotFound }, // ruta comodín:'**' al componente NotFound
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Home } from './features/home/home';
+import { MovieDetails } from './features/movie-details/movie-details';
+import { MovieList } from './features/movie-list/movie-list';
+import { NotFound } from './features/not-found/not-found';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Home,
+    MovieDetails,
+    MovieList,
+    NotFound,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('movie-app');
